6ec6fa4e6be4ca011d1f756a37fb88befecf6535
mindkeeper <fcb.nyak@gmail.com>
2022-09-22 15:36:12 +0700

    commit tugas 1

---
+++ task1.js
@@ -0,0 +1,23 @@
+const biodata = {
+  name: "Nur Cholis Majid",
+  age: 24,
+  hobbies: ["Gaming", "Badminton", "coding"],
+  isMarried: false,
+  schoolList: [
+    { name: "SDN Sapeken IV", yearIn: 2004, yearOut: 2010, major: null },
+    { name: "SMP Negeri 1 Sumenep", yearIn: 2010, yearOut: 2013, major: null },
+    { name: "SMA Negeri 1 Sumenep", yearIn: 2013, yearOut: 2016, major: "IPA" },
+    {
+      name: "Universitas Negeri Malang",
+      yearIn: 2016,
+      yearOut: 2022,
+      major: "Teknik Informatika",
+    },
+  ],
+  skills: [
+    { skillName: "Javascript", level: "Beginner" },
+    { skillName: "kotlin", level: "Beginner" },
+    { skillName: "Android Studio", level: "Beginner" },
+  ],
+  interestedInCoding: true,
+};
