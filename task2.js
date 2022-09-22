95479b5afd0111a7378401a52630da6cc2c3d130
mindkeeper <fcb.nyak@gmail.com>
2022-09-22 23:31:52 +0700

    revisi pertama, penambahan pengecekan jika number tidak sama dengan 0

--- task2.js
+++ task2.js
@@ -1,9 +1,13 @@
 const checkGrade = (mtk, bahasa, english, ipa) => {
   if (
     typeof mtk === "number" &&
+    mtk >= 0 &&
     typeof bahasa === "number" &&
+    bahasa >= 0 &&
     typeof english === "number" &&
-    typeof ipa === "number"
+    english >= 0 &&
+    typeof ipa === "number" &&
+    ipa >= 0
   ) {
     const average = (mtk + bahasa + english + ipa) / 4;
     const grade =
@@ -19,6 +23,7 @@ const checkGrade = (mtk, bahasa, english, ipa) => {
     return `Rata-rata = ${average}
     Grade = ${grade}`;
   } else {
-    return "Data Harus Number";
+    return "Input Invalid";
   }
 };
+console.log(checkGrade(10, 20, 20, -20));
